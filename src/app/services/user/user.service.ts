ae50e1370f7c64b8ac172c7091e773a3ceb3e80f
Camila <camila.matosrocha1234@gmail.com>
2024-05-01 18:01:21 -0300

    feat: auth guard implementation

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/app/environments/environment';
-import { UserSignupRequest } from '../../interfaces/user/user-signup-request';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
-import { UserSignupResponse } from '../../interfaces/user/user-signup-response';
+import { environment } from 'src/app/environments/environment';
 import { AuthRequest } from 'src/app/interfaces/user/auth/auth-request';
 import { AuthResponse } from 'src/app/interfaces/user/auth/auth-response';
+import { UserSignupRequest } from '../../interfaces/user/user-signup-request';
+import { UserSignupResponse } from '../../interfaces/user/user-signup-response';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +14,7 @@ import { AuthResponse } from 'src/app/interfaces/user/auth/auth-response';
 export class UserService {
   private API_URL = environment.API_URL;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cookie: CookieService) {}
 
   userSignUp(payload: UserSignupRequest): Observable<UserSignupResponse> {
     return this.http.post<UserSignupResponse>(`${this.API_URL}/user`, payload);
@@ -21,5 +22,10 @@ export class UserService {
 
   authUser(payload: AuthRequest): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.API_URL}/auth`, payload);
+  }
+
+  isLoggedIn(): boolean {
+    const jwtToken = this.cookie.get('USER_INFO');
+    return jwtToken ? true : false;
   }
 }
